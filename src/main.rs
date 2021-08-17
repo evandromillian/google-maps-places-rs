64d286b822b97145c6237e42df26b2b899283d3a
Jon Saw <jon.saw@gmail.com>
2021-08-17 11:59:31 +0800

    update main example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
 
-use google_maps_places::places::Places;
+use google_maps_places::{Places, Response};
 
 fn main() {
     let api_key = match env::var("GOOGLE_MAPS_API_KEY") {
@@ -19,8 +19,9 @@ fn main() {
 
     let place_id = &args[1];
 
-    let places = Places { api_key: api_key };
-    let place = match places.get_map_place(place_id) {
+    let places = &Places { api_key: &api_key };
+
+    let res = match places.get_map_place(place_id) {
         Ok(b) => b,
         Err(e) => {
             println!("Error {:?}", e);
@@ -28,5 +29,35 @@ fn main() {
         }
     };
 
-    println!("Place: \n{:?}", place);
+    match res {
+        Response::OK { result } => {
+            println!("id                : {}", result.place_id);
+            println!("name              : {}", result.name);
+            println!("formatted_address : {}", result.formatted_address);
+            println!("");
+
+            println!("street_number : {}", result.street_number().unwrap_or(""),);
+            println!("route         : {}", result.route().unwrap_or(""));
+            println!("sublocality   : {}", result.sublocality().unwrap_or(""));
+            println!("postal_code   : {}", result.postal_code().unwrap_or(""));
+            println!("city          : {}", result.city().unwrap_or(""));
+            println!("state         : {}", result.state().unwrap_or(""));
+            println!("country       : {}", result.country().unwrap_or(""));
+        }
+        Response::ZeroResults => {
+            println!("Zero results");
+        }
+        Response::InvalidRequest => {
+            println!("Invalid Request");
+        }
+        Response::OverQueryLimit => {
+            println!("Over Query Limit");
+        }
+        Response::RequestDenied { error_message } => {
+            println!("Request Denied: {}", error_message);
+        }
+        Response::UnknownError => {
+            println!("Unknown Error")
+        }
+    };
 }
